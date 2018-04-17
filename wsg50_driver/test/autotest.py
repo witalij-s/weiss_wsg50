196e5beb470e39638e25c658680b07e3829b5709
Zaki <rfa-mr@ipa.fraunhofer.de>
2018-04-17 19:36:45 +0200

    added more tests

--- wsg50_driver/test/autotest.py
+++ wsg50_driver/test/autotest.py
@@ -8,6 +8,7 @@ import json                 # Json API
 import rospy                # ROS API
 import rostest				# ROS specific testing library
 import unittest				# Python generic testing library used by ROS
+import time
 from std_srvs.srv import *
 from wsg50_common.srv import *     # For loading service
 
@@ -15,14 +16,96 @@ from wsg50_common.srv import *     # For loading service
 class TestGripper(unittest.TestCase):
 	# Each test case is individually defined as a function
 
-	# Test a large program
-	def test_autotest(self):
+	# # Test Moving
+	# def test_moving(self):
+	# 	rospy.wait_for_service('/wsg50_driver/move')
+	# 	srv_move = rospy.ServiceProxy('/wsg50_driver/move', Move)
+	# 	res = srv_move(25, 15)
+	# 	assert(res.error == 0)
+
+	# # Test Homing
+	# def test_homing(self):
+	# 	rospy.wait_for_service('/wsg50_driver/homing')
+	# 	srv_home = rospy.ServiceProxy('/wsg50_driver/homing', Empty)
+	# 	res = srv_home()
+
+	# Test Delta/Icremental Moving - close
+	def test_continuous_close(self):
 		rospy.wait_for_service('/wsg50_driver/homing')
-		rospy.wait_for_service('/wsg50_driver/move')
 		srv_home = rospy.ServiceProxy('/wsg50_driver/homing', Empty)
+		res = srv_home()
+
+		rospy.wait_for_service('/wsg50_driver/move_incrementally')
+		srv_delta_move = rospy.ServiceProxy('/wsg50_driver/move_incrementally', Incr)
+		for i in range(0, 4):
+			res = srv_delta_move('close', 15)
+			assert(res.error == 0)
+
+	# Test Delta/Icremental Moving - open
+	def test_continuous_open(self):		
+		rospy.wait_for_service('/wsg50_driver/move')
 		srv_move = rospy.ServiceProxy('/wsg50_driver/move', Move)
-		srv_home()
-		srv_move(0, 10)
+		res = srv_move(0, 20)
+
+		rospy.wait_for_service('/wsg50_driver/move_incrementally')
+		srv_delta_move = rospy.ServiceProxy('/wsg50_driver/move_incrementally', Incr)
+		for i in range(0, 4):
+			res = srv_delta_move('open', 15)
+			assert(res.error == 0)
+
+	# Test Delta/Icremental Moving - open
+	def test_close_open(self):		
+		rospy.wait_for_service('/wsg50_driver/homing')
+		srv_home = rospy.ServiceProxy('/wsg50_driver/homing', Empty)
+		res = srv_home()
+
+		rospy.wait_for_service('/wsg50_driver/move_incrementally')
+		srv_delta_move = rospy.ServiceProxy('/wsg50_driver/move_incrementally', Incr)
+
+		res = srv_delta_move('close', 15)
+		assert(res.error == 0)
+
+		res = srv_delta_move('close', 15)
+		assert(res.error == 0)
+
+		res = srv_delta_move('open', 15)
+		assert(res.error == 0)
+
+		res = srv_delta_move('open', 15)
+		assert(res.error == 0)
+
+		res = srv_delta_move('close', 15)
+		assert(res.error == 0)
+
+	# Test Grasping/Releasing with no object to grasp
+	def test_grasp_release(self):
+		rospy.wait_for_service('/wsg50_driver/homing')
+		srv_home = rospy.ServiceProxy('/wsg50_driver/homing', Empty)
+		res = srv_home()
+
+		rospy.wait_for_service('/wsg50_driver/grasp')
+		srv_grasp = rospy.ServiceProxy('/wsg50_driver/grasp', Move)
+		res = srv_grasp(30, 15)
+		assert(res.error != 0)
+
+		rospy.wait_for_service('/wsg50_driver/release')
+		srv_release = rospy.ServiceProxy('/wsg50_driver/release', Move)
+		res = srv_release(100, 15)
+		assert(res.error == 0)
+	
+	# # Test Grasping
+	# def test_grasping(self):
+	# 	rospy.wait_for_service('/wsg50_driver/grasp')
+	# 	srv_grasp = rospy.ServiceProxy('/wsg50_driver/grasp', Move)
+	# 	res = srv_grasp(30, 15)
+	# 	assert(res.error != 0)
+
+	# # Test Releasing
+	# def test_releasing(self):
+	# 	rospy.wait_for_service('/wsg50_driver/release')
+	# 	srv_release = rospy.ServiceProxy('/wsg50_driver/release', Move)
+	# 	res = srv_release(100, 15)
+	# 	assert(res.error != 0)
 
 	# --------------------------------------------
 	# Extend it with new tests here
